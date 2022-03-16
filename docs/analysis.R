d242f3ac2424cfa29b6244467bf893f40d980745
fatimaalsinai <fatimaalsinai@gmail.com>
2022-03-16 16:04:21 -0700

    index rmd knits and I can see my charts. I changed my varaibles and compared races instead of genders because it was easier for me to compare.

--- docs/analysis.R
+++ docs/analysis.R
@@ -15,62 +15,61 @@ setwd("~/_Code/a3-falsinai")
 # Load incarceration_trends.csv data
 
 incarceration_trends <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
-# view(incarceration_trends)
+view(incarceration_trends)
 
 # Variable: Female Jail Population 
 # What is the most recent average value of female jail population across all counties? 
-recent_average_female_jail_pop <- incarceration_trends %>%
+recent_average_latinx_pop <- incarceration_trends %>%
   filter(year == max(year, na.rm = T)) %>% 
-  summarize(mean_female_jail = mean(female_jail_pop, na.rm = T))
+  summarize(mean_latinx_jail = mean(latinx_jail_pop, na.rm = T))
 
 # From the dataset, extract the year that has the highest female jail population
-year_highest_female_jail_pop <- incarceration_trends %>%
-  filter(female_jail_pop == max(female_jail_pop, na.rm = T)) %>% 
+year_highest_latinx_jail_pop <- incarceration_trends %>%
+  filter(latinx_jail_pop == max(latinx_jail_pop, na.rm = T)) %>% 
   pull (year)
 
 # From the dataset, extract the state with the highest female jail population 
-place_highest_female_jail_pop <- incarceration_trends %>% 
-  filter(female_jail_pop == max(female_jail_pop, na.rm = T)) %>%
+place_highest_latinx_jail_pop <- incarceration_trends %>% 
+  filter(latinx_jail_pop == max(latinx_jail_pop, na.rm = T)) %>%
   select(state, county_name)
 
 # What is the total female jail population in Washington in the most recent year?
 total_wa <- incarceration_trends %>%
   filter(state == "WA") %>%
   filter(year == max(year, na.rm = T)) %>%
-  summarize(total_female_jail = sum(female_jail_pop, na.rm = T))
+  summarize(total_latinx_jail = sum(latinx_jail_pop, na.rm = T))
   
 # What are the top five counties with the highest female jail population 
 top_5_places <- incarceration_trends %>%
   group_by(county_name) %>% 
-  summarize(total_in_each_county = sum(female_jail_pop, na.rm = T)) %>% 
+  summarize(total_in_each_county = sum(latinx_jail_pop, na.rm = T)) %>% 
   arrange(desc(total_in_each_county)) %>%
   slice(1:5)
 top_5_chart <- top_5_places %>% 
   left_join(incarceration_trends, by = "county_name", na.rm = T) %>%
-  filter(female_jail_pop != "NA")
+  filter(latinx_jail_pop != "NA")
 
 # Trends over time chart 
-female_jail_pop_over_time_in_top_5 <- ggplot(data = top_5_chart) +
-  geom_point(mapping = aes(x= year, y= female_jail_pop, color = county_name)) +
-  geom_smooth(mapping = aes(x = year, y = female_jail_pop, color = county_name),
+latinx_jail_pop_over_time_in_top_5 <- ggplot(data = top_5_chart) +
+  geom_point(mapping = aes(x= year, y= latinx_jail_pop, color = county_name)) +
+  geom_smooth(mapping = aes(x = year, y = latinx_jail_pop, color = county_name),
               se = FALSE) +
-  labs(x = "Year", y = "Female Jail Population", title = "Top 5 Counties With The Highest 
-       Female Jail Population Over Time") +
+  labs(x = "Year", y = "Latinx Jail Population", title = "Top 5 Counties With The Highest Latinx Jail Population Over Time") +
     scale_color_discrete("County Name")
   
 # Variable comparison charts
 # Make a data frame for female and male pop
-two_variables_data <- data.frame(incarceration_trends$female_jail_pop,
-                                 incarceration_trends$male_jail_pop)
+two_variables_data <- data.frame(incarceration_trends$latinx_jail_pop,
+                                 incarceration_trends$white_jail_pop)
 #Graph the two variables
 variable_comparison_chart <-  ggplot(data = two_variables_data) + 
-  geom_point(mapping = aes(x= incarceration_trends.female_jail_pop,
-                           y = incarceration_trends.male_jail_pop)) +
-  labs(x= "Female Jail Pop", y = "Male Jail pop", title = "Female Jail Population VS Male Jail Population")
+  geom_point(mapping = aes(x= incarceration_trends.latinx_jail_pop,
+                           y = incarceration_trends.white_jail_pop)) +
+  labs(x= "Latinx Jail Pop", y = "White Jail pop", title = "Latinx Jail Population VS White Jail Population")
 
 
 # Map
-female_jail_data <- incarceration_trends %>%
+latinx_jail_data <- incarceration_trends %>%
   filter(year == max(year, na.rm = T))
 
 # Join 'incarceration_trends' dataset with the map_data using the map_data function
@@ -81,8 +80,8 @@ join_map <- map_data("county") %>%
 # Merge map data and incarceration data
 
 merge_map <- join_map %>%
-  left_join(female_jail_data, by = "fips") %>% 
-  filter(female_jail_pop != "NA")
+  left_join(latinx_jail_data, by = "fips") %>% 
+  filter(latinx_jail_pop != "NA")
 
 
 # Structuring blank theme 
@@ -99,13 +98,13 @@ blank_theme <- theme_bw() +
     panel.border = element_blank(),
   )
 # Creating map for female in jail 'female_jail_map'
-female_jail_map <- ggplot(merge_map) +
+latinx_jail_map <- ggplot(merge_map) +
   geom_polygon(
-    mapping = aes(x= long, y= lat, group = group, fill = female_jail_pop),
+    mapping = aes(x= long, y= lat, group = group, fill = latinx_jail_pop),
     color = "gray", size = 0.3) +
   coord_map () +
-  scale_fill_continuous(limits = c(0,max(merge_map$female_jail_pop)),
+  scale_fill_continuous(limits = c(0,max(merge_map$latinx_jail_pop)),
                         na.value = "white", low = "yellow", high = "red") +
   blank_theme +
-  ggtitle("Female Jail Population in the U.S.")
+  ggtitle("Latinx Jail Population in the U.S.")
 
